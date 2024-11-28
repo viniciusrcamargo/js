987d97878cbee20cf7f47b07c13c48e77c1ff43a
Vinicius <viniciusr.camargo@gmail.com>
2024-11-28 11:02:23 -0300

    Array Splice

---
+++ Curso Javascript Conhecendo Arrays/atualizarLista.js
@@ -0,0 +1,6 @@
+const listaEstudantes = ['João', 'Ana', 'Alex', 'Caio', 'Breno', 'Vanda'];
+
+listaEstudantes.splice(1, 2);//start /deleta quantos elementos / a partir de 'Alex'
+//remove elementos do array
+
+console.log(listaEstudantes);
