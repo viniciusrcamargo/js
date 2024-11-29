6928a7b9a8a2e2107ee3fab3d9a31cd3801ab295
Vinicius <viniciusr.camargo@gmail.com>
2024-11-29 11:03:00 -0300

    Array Splice

---
+++ Curso Javascript Conhecendo Arrays/dividindoArray.js
@@ -0,0 +1,3 @@
+const listaEstudantes = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'André', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinícius', 'Renan', 'Renata', 'Daisy', 'Camilo'];
+
+const sala1 = listaEstudantes.slice(0, listaEstudantes.length / 2);
