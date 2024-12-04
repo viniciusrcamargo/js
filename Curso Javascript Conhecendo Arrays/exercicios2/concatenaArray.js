15fbf707b79127106aa30757cfdeba149fafae61
Vinicius <viniciusr.camargo@gmail.com>
2024-12-04 19:09:51 -0300

    Praticando exercícios

---
+++ Curso Javascript Conhecendo Arrays/exercicios2/concatenaArray.js
@@ -0,0 +1,6 @@
+const nomes = ['Viniius','Cinder','Patricia', 'Leticia', 'Julia'];
+const idades = [25, 30, 16, 22, 18];
+
+const concatenaPessoas = [nomes, idades];
+
+console.log(concatenaPessoas);
