56789b1399fc2f00a6d886b8734505a8fe8fd7ce
Vinicius <viniciusr.camargo@gmail.com>
2024-12-05 19:10:18 -0300

    Praticando exercícios

---
+++ Curso Javascript Conhecendo Arrays/exercicios2/concatenarMenus.js
@@ -0,0 +1,6 @@
+const menuPrincipal = ['Massas', 'Lasagna', 'Peru', 'Pizza']
+const menuSobremesas = ['Bolo', 'Sorvete', 'Torta', 'Pudim']
+
+const menuCompleto = menuPrincipal.concat(menuPrincipal,menuSobremesas)
+
+console.log(menuCompleto)
