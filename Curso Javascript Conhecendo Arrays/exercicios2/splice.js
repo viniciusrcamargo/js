041278ee689e497c01c5a4a933bd88b728135178
Vinicius <viniciusr.camargo@gmail.com>
2024-12-07 19:10:55 -0300

    Praticando exercícios

---
+++ Curso Javascript Conhecendo Arrays/exercicios2/splice.js
@@ -0,0 +1,6 @@
+const frutas = ['Maçã', 'Banana', 'Laranja', 'Limão', 'Abacaxi'];
+
+const removeFrutas = frutas.splice(2,2,"Kiwi", "Pessego");
+
+console.log(frutas);    
+console.log(removeFrutas);
