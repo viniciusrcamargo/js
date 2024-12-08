c5b619fa124fbe55ef8dbceda5f2af8d60f3f55a
Vinicius <viniciusr.camargo@gmail.com>
2024-12-08 19:29:56 -0300

    Praticando exercícios

---
+++ Curso Javascript Conhecendo Arrays/exercicios2/matriz.js
@@ -0,0 +1,9 @@
+let matriz = []
+
+matriz.push([1,2,3], [4,5,6], [7,8,9])
+
+for(let i = 0; i < matriz.length; i++) {
+    for(let j = 0; j < matriz[i].length; j++) {
+       console.log(matriz[i][j]);
+    }
+}
