354df13cc370c62d8c82a3a97842d17d857b105c
Vinicius <viniciusr.camargo@gmail.com>
2024-12-13 21:25:27 -0300

    Clonando array

---
+++ Curso Javascript Conhecendo Arrays/clonar.js
@@ -0,0 +1,9 @@
+const notas = [7,7,8,9];
+const novaListaNotas = [...notas];
+
+//spread operator
+novaListaNotas.push(10);
+
+console.log(`As notas são ${notas}`)
+console.log(`As novas notas são ${novaListaNotas}`)
+
