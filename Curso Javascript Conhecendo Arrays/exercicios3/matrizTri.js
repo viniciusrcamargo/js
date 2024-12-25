fd578986c8a0136175b6e42b2b7c4868ad40113f
Vinicius <viniciusr.camargo@gmail.com>
2024-12-25 08:53:59 -0300

    Estudando arrays

--- Curso Javascript Conhecendo Arrays/exercicios3/matrizTri.js
+++ Curso Javascript Conhecendo Arrays/exercicios3/matrizTri.js
@@ -1,11 +1,18 @@
 let alunosIdadesNotas = [
-    ['João','José','Maria','Ana'],
-    [15,17,14,16],
-    [8,7,9.5,8.5]
+    [
+        ['João','José','Maria','Ana'],
+    ]
+    [
+        [15,17,14,16],
+        [8,7,9.5,8.5]
+    ]
+    
 ]
 
 for(let i = 0; i < alunosIdadesNotas.length; i++ ){
     for(let j = 0; j < alunosIdadesNotas[i].length; j++){
-        console.log(alunosIdadesNotas[i][j])
+        for(let k = 0; k < alunosIdadesNotas[i][j].length; k++){
+            console.log(alunosIdadesNotas[i][j][k])
+        }
     }
 }
