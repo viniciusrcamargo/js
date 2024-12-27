516041bc538c596d17ba00a61731fd66e3838f38
Vinicius <viniciusr.camargo@gmail.com>
2024-12-27 08:59:01 -0300

    Estudando arrays

--- Curso Javascript Conhecendo Arrays/exercicios3/matrizTri.js
+++ Curso Javascript Conhecendo Arrays/exercicios3/matrizTri.js
@@ -10,11 +10,11 @@ let alunosIdadesNotas = [
 ]
 
 for(let i = 0; i < alunosIdadesNotas.length; i++ ){
-    console.log('O aluno ${alunosIdadesNotas[i]}')
+    console.log(`O aluno ${alunosIdadesNotas[i]}`)
     for(let j = 0; j < alunosIdadesNotas[i].length; j++){
-        console.log('A idade do aluno ${alunosIdadesNotas[i][j]}')
+        console.log(`A idade do aluno ${alunosIdadesNotas[i][j]}`)
         for(let k = 0; k < alunosIdadesNotas[i][j].length; k++){
-            console.log('A nota do aluno ${alunosIdadesNotas[i][j][k]}')
+            console.log(`A nota do aluno ${alunosIdadesNotas[i][j][k]}`)
         }
     }
 }
