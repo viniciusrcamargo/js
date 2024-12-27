f1cf4513c0f2d65bc6f22a2730e319fa6304b771
Vinicius <viniciusr.camargo@gmail.com>
2024-12-27 08:57:46 -0300

    Estudando arrays

--- Curso Javascript Conhecendo Arrays/exercicios3/matrizTri.js
+++ Curso Javascript Conhecendo Arrays/exercicios3/matrizTri.js
@@ -4,15 +4,17 @@ let alunosIdadesNotas = [
     ],
     [
         [15,17,14,16],
-        [8,7,9.5,8.5]
+        [8,7,9.5,10]
     ]
     
 ]
 
 for(let i = 0; i < alunosIdadesNotas.length; i++ ){
+    console.log('O aluno ${alunosIdadesNotas[i]}')
     for(let j = 0; j < alunosIdadesNotas[i].length; j++){
+        console.log('A idade do aluno ${alunosIdadesNotas[i][j]}')
         for(let k = 0; k < alunosIdadesNotas[i][j].length; k++){
-            console.log(alunosIdadesNotas[i][j][k])
+            console.log('A nota do aluno ${alunosIdadesNotas[i][j][k]}')
         }
     }
 }
