874778e5e532f01498d3161285f740801b65a4b3
Vinicius <viniciusr.camargo@gmail.com>
2025-01-14 16:19:38 -0300

    Conhecendo objetos no JS

---
+++ Curso conhecendo objetos/projetoCurso/objeto.js
@@ -0,0 +1,10 @@
+const listaCPFs = ['12312312312', '11155522245'];
+
+const objPessoa = {
+    nome: 'Vinicius Rodrigues Camargo',
+    idade: 39,
+    cpf: 12312312311,
+    turma: 'Dart'
+}
+
+console.log(objPessoa);
