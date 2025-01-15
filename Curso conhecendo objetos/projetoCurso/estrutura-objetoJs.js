e2180bb0c5fcf97082de25c3736892c356cda7ba
Vinicius <viniciusr.camargo@gmail.com>
2025-01-15 16:29:27 -0300

    Acessando objetos

---
+++ Curso conhecendo objetos/projetoCurso/estrutura-objetoJs.js
@@ -0,0 +1,9 @@
+const estudante = {
+    nome: 'Vinicius Rodrigues Camargo',
+    idade: 39,
+    cpf: 12312312311,
+    turma: 'Dart'
+}
+
+console.log(estudante.nome);
+console.log(`O nome do estudante é ${estudante.nome}`);
