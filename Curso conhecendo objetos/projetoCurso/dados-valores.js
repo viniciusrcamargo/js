d26acf8fc9f42269c55727391e3fdd46d464efc2
Vinicius <viniciusr.camargo@gmail.com>
2025-03-30 21:02:24 -0300

    Exercícios de objetos

---
+++ Curso conhecendo objetos/projetoCurso/dados-valores.js
@@ -0,0 +1,9 @@
+const estudante = {
+    nome: 'Maria',
+    idade: 39,
+    cpf: '123.456.789-00',
+    bolsista: true,
+    cursos: ['Desenvolvimento Web', 'Análise de Dados', 'Estatística']
+}
+
+console.log(estudante.cursos); // Saída: Análise de Dados
